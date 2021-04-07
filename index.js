acba30d9947cece3205fcf05358d4d6ea50d68f2
Aqeel Aloraibi <aqeel.aloraibi@gmail.com>
2021-04-07 17:10:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
+<<<<<<< HEAD
 let companyCeo = 'Susan Smith';
 let mostProfitableNeighborhood ="Chelsea"
+=======
+let 
+>>>>>>> d99664fcad944ad2a62e3fb19bbef00c7c191770
 const companyName = 'Scuber';
